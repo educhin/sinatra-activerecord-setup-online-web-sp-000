7ed26b946c203d5bd7ec8b5c770bd358d74cbd82
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-15 14:22:58 +0000

    Automatically backed up by Learn

--- db/migrate/20190815141610_create_dogs.rb
+++ db/migrate/20190815141610_create_dogs.rb
@@ -9,4 +9,12 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   def down
     drop_table :dogs
   end
+  # The change method is actually a shorter way of writing up and down methods. We can refactor our migration to look like this:
+  # def change
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  # While the rollback (down) method is not included, it's implicit in the change method. Rolling back the database would work in exactly the same way as using the down method.
 end
